a3e51b556489047d8220c74111db16485f006ecd
Yotam Laufer <yotam@youtab.me>
2015-07-28 19:49:15 +0300

    - caged transformations

--- src/js/build/parsers/FrettedInstrumentParser.js
+++ src/js/build/parsers/FrettedInstrumentParser.js
@@ -1,5 +1,7 @@
 var _ = require('underscore');
 var string = require('underscore.string');
+var teoria = require('teoria');
+
 var CSVParser = require('./CSVParser');
 var ValueParser = require('./ValueParser');
 /**
@@ -9,6 +11,12 @@ FrettedInstrumentParser = function () {
     this.instrument_ = null;
     this.tuning_ = null;
     this.callback_ = null;
+
+    /**
+     * @type {Array.<string> | null}
+     * @private
+     */
+    this.openStrings_ = [];
 
     /**
      * @dict
@@ -48,6 +56,11 @@ FrettedInstrumentParser.prototype.parse = function(instrument, tuning, csv, call
         this.done_(null, []);
         return this;
     }
+
+    this.openStrings_ = _.map(this.tuning_.split('-'), function(noteWithOctave) {
+        return noteWithOctave.substring(0, noteWithOctave.length - 1);
+    }, this);
+
     this.csvParser_.parse(csv, _.bind(this.assemble_, this));
     return this;
 };
@@ -72,6 +85,7 @@ FrettedInstrumentParser.prototype.createGAGEDSources_ = function(sourceChordList
     _.forEach(sourceChordList, function(sourceChord) {
         var frets = sourceChord.alternateFrets || sourceChord.frets;
         var fingering = sourceChord.alternateFingering || sourceChord.frets;
+        var fingeringInterpolation = sourceChord.alternateFingering ? 0 : 1;
         if (sourceChord.name && frets) {
             var rootContainer = this.cagedSourceChords_[sourceChord.root];
             if (!rootContainer) {
@@ -79,14 +93,70 @@ FrettedInstrumentParser.prototype.createGAGEDSources_ = function(sourceChordList
             }
             _.forEach(sourceChord.chords, function(chord) {
                 rootContainer[chord] = {
+                    frets: frets,
                     fingering: fingering,
-                    frets: frets
+                    fingeringInterpolation: fingeringInterpolation
                 };
             })
         }
     }, this);
 
-    console.log(JSON.stringify(this.cagedSourceChords_, null, 2));
+    this.transform('D', '7', 'A');
+
+    // console.log(JSON.stringify(this.cagedSourceChords_, null, 2));
+};
+
+/**
+ * @param {string} root of the source chord
+ * @param {string} chord quality
+ * @param {string} caged shape
+ * @return {*}
+ */
+FrettedInstrumentParser.prototype.transform = function(root, chord, caged) {
+    var rootMap = this.cagedSourceChords_[caged];
+    if (!rootMap) {
+        console.warn('no root map found');
+        return null;
+    }
+
+    var cagedChord = rootMap[chord];
+    if (!cagedChord) {
+        console.warn('no chord found');
+        return null;
+    }
+
+    var stringNum = -1;
+    var rootFret = _.find(cagedChord.frets, function(fret) {
+        stringNum++;
+        return fret >= 0;
+    }, this);
+
+    if (_.isUndefined(rootFret)) {
+        console.warn('no string found');
+        return null;
+    }
+
+    var distance = this.getDistance(this.openStrings_[stringNum], root) - rootFret;
+    var transposed = this.transpose(cagedChord.frets, distance);
+
+    console.log('interpolated', transposed);
+};
+
+FrettedInstrumentParser.prototype.transpose = function(frets, distance) {
+    return _.map(frets, function(fret) {
+        if (fret < 0) {
+            return fret;
+        }
+        return fret + distance;
+    });
+};
+
+FrettedInstrumentParser.prototype.getDistance = function(start, end) {
+    var interval = teoria.note(start).interval(teoria.note(end)).semitones();
+    if (interval < 0) {
+        interval += 12;
+    }
+    return interval;
 };
 
 FrettedInstrumentParser.prototype.assembleGroups_ = function(row, index, rows) {
@@ -109,7 +179,6 @@ FrettedInstrumentParser.prototype.summarize_ = function() {
         })
     }, this)
 };
-
 
 FrettedInstrumentParser.prototype.done_ = function(error, data) {
     this.callback_(error || null, {
